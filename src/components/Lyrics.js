931522f5eadb1b961bd134b30ef2769c016b6dc7
Christopher Arsenault <christopher.arsenault82@gmail.com>
2018-09-08 09:44:25 -0400

    added more search results messages and finished setlist

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Lyrics extends Component {
     render() {
         return (
-            <div>
+            <div className='lyricsPane'>
                 <p>{this.props.lyrics}</p>
             </div>
         );
--- src/components/Setlist.js
+++ src/components/Setlist.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
-
 //show the artist and track from app.js state 
 
-
-const SetList = (props) => {
+class setList extends Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+    handleClick = (e) => {
+        this.props.getLyrics(e.target.className, e.target.id)
+    }
+    render() {
         return (
             <div>
                 <h2>Setlist</h2>
-                <ul className="setList">
-                    {props.setList !== null ? Object.keys(props.setList).map((key) => {
-                        return <div className="setListItem" key={key}>
-                            <li key={key} onClick={()=> this.props.getLyrics(props.setList[key].artist, props.setList[key].track)}>
-                                {props.setList[key].artist} {props.setList[key].track}
-                            </li>
-                            {/* <button className="deleteGameButton" onClick={() => this.props.removeFromList(key)}>Delete</button> */}
-                        </div>
-                    }//return 
-                    )//map
-                        : "You have no saved songs."}
+                <ul className='setList'>
+                    {this.props.setList !== null ? Object.keys(this.props.setList).map((key) => {
+                        return (
+                            <div className='setListItem' key={key}>
+                                <li key={key} className={this.props.setList[key].artist} id={this.props.setList[key].track} onClick={this.handleClick}>
+                                    {this.props.setList[key].artist} - {this.props.setList[key].track}
+                                </li>
+                            {/* <button className='deleteGameButton' onClick={() => this.props.removeFromList(key)}>Delete</button> */}
+                            </div>
+                        )
+                    })//map
+                        : 'You have no saved songs.'
+                    }
                 </ul>
             </div>
         );
     }
+};
 
-
-export default SetList;
+export default setList;
